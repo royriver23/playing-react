3df9bd6c029a71f87a4943263e61233e21e51a07
Roy Rivera <roy.fita23@gmail.com>
2018-02-18 23:08:08 -0600

    Understanding Lifecycle Hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-      key={person.id}
-      click={() => {props.clicked(index)}}
-      name={person.name}
-      age={person.age}
-      changed={(event) => {props.changed(event, person.id)}} />
-    })
+class Persons extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[Persons.js] Inside constructor', props);
+  }
 
-export default persons;
+  componentWillMount() {
+    console.log('[Persons.js] Inside componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] Inside componentDidMount');
+  }
+
+  render() {
+    console.log('[Persons.js] Inside render');
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        click={() => {this.props.clicked(index)}}
+        name={person.name}
+        age={person.age}
+        changed={(event) => {this.props.changed(event, person.id)}} />
+    });
+  }
+}
+
+export default Persons;
